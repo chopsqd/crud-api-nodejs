ce8f90c041b790415e836911dee0e1b2afa05c9b
Michael Chop <007micle007@gmail.com>
2022-11-17 17:18:58 +0300

    Added a DELETE request to the project

--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,56 @@ let users = [
 
 // --> /users/
 router.get('/', (req, res) => {
-    res.send(users)
+    try {
+        res.send(users)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Some error occurred'
+        })
+    }
+})
+
+router.get('/:id', (req, res) => {
+    try {
+        const {id} = req.params
+        const foundUser = users.find(user => user.id === id)
+
+        res.send(foundUser)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Some error occurred'
+        })
+    }
 })
 
 router.post('/', (req, res) => {
-    const user = {...req.body, id: uuid()}
-    users.push(user)
+    try {
+        const user = {...req.body, id: uuid()}
+        users.push(user)
 
-    res.send(`User ${user.firstName} added to the database!`)
+        res.send(`User ${user.firstName} added to the database!`)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Some error occurred'
+        })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    try {
+        const {id} = req.params
+        users = users.filter(user => user.id !== id)
+
+        res.send(`User with the id:${id} deleted from the database!`)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Some error occurred'
+        })
+    }
 })
 
 export default router
