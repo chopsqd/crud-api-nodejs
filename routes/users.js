7384aa818c627047f49b0af51fe055bdfff67700
Michael Chop <007micle007@gmail.com>
2022-11-17 17:00:33 +0300

    Added a POST request to the project

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
 import express from 'express'
+import {v4 as uuid} from 'uuid'
+
 
 const router = express.Router()
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 24
-    }
+let users = [
+
 ]
 
 // --> /users/
 router.get('/', (req, res) => {
     res.send(users)
+})
+
+router.post('/', (req, res) => {
+    const user = {...req.body, id: uuid()}
+    users.push(user)
+
+    res.send(`User ${user.firstName} added to the database!`)
 })
 
 export default router
